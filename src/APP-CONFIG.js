65da6d8e62619c236146ff4028627ff54e88e251
Sergioamjr <sergioamjr91@gmail.com>
2019-03-10 16:29:08 -0300

    Changed backend end production serve

--- src/APP-CONFIG.js
+++ src/APP-CONFIG.js
@@ -1,6 +1,5 @@
 export const IS_PRODUCTION = !/localhost/.test(window.location.host);
-// export const BACKEND = "http://localhost:5000/api";
 
 export const BACKEND = /localhost/.test(window.location.host)
-  ? "http://localhost:3333"
-  : "https://cst-fearless-hyrax.cfapps.io/api";
+  ? 'http://localhost:3333'
+  : 'https://switchblade-service.herokuapp.com';
